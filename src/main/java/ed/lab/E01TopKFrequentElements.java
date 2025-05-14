294f7773fea08634bb307fe9114176124a88a2bc
FernandaTurcios <jfturcios@correo.url.edu.gt>
2025-05-13 23:13:10 -0600

    Ejercicio 1

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,34 @@
 package ed.lab;
+import java.util.*;
 
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            buckets[freq].add(num);
+        }
+
+        int[] result = new int[k];
+        int idx = 0;
+        for (int freq = buckets.length - 1; freq >= 0 && idx < k; freq--) {
+            for (int num : buckets[freq]) {
+                result[idx++] = num;
+                if (idx == k) {
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
+
