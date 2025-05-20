ecec12976db6bcc0973783dc786e9baa25f9b6f5
FernandaTurcios <jfturcios@correo.url.edu.gt>
2025-05-19 19:27:14 -0600

    Ejercicio 2

--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,41 @@
 package ed.lab;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class E02AutocompleteSystem {
+    private final Map<String, Integer> sentenceCount;
+    private String currentInput;
 
     public E02AutocompleteSystem(String[] sentences, int[] times) {
-
+        sentenceCount = new HashMap<>();
+        currentInput = "";
+        for (int i = 0; i < sentences.length; i++) {
+            sentenceCount.merge(sentences[i], times[i], Integer::sum);
+        }
     }
 
     public List<String> input(char c) {
-        return null;
+        if (c == '#') {
+            sentenceCount.merge(currentInput, 1, Integer::sum);
+            currentInput = "";
+            return Collections.emptyList();
+        }
+
+        currentInput += c;
+        String prefix = currentInput;
+
+        return sentenceCount.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(prefix))
+                .sorted((e1, e2) -> {
+                    int freqCompare = e2.getValue().compareTo(e1.getValue());
+                    if (freqCompare != 0) {
+                        return freqCompare;
+                    }
+                    return e1.getKey().compareTo(e2.getKey());
+                })
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
